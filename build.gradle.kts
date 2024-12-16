c24a1fe062e7608e63dfa4ba71d69dc425326fe9
lawb4 <yahorlabanau@gmail.com>
2024-12-16 08:41:42 +0300

    add config files

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.junit:junit-bom:5.10.0"))
+    implementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36") // Added annotation processor
+    implementation("io.rest-assured:rest-assured:5.5.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0-rc1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
